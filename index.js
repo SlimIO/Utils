4854b453cc215dcfa464c412b1870ceb6993e0e4
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2018-07-07 14:28:44 +0200

    Update npm packages

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 /**
  * @namespace utils
  */
-// Require third-party dependencies
-const is = require("@sindresorhus/is");
 
 /**
  * @exports utils/taggedString
@@ -11,10 +9,10 @@ const is = require("@sindresorhus/is");
  * @param {!String} chaines initial string
  * @param {any[]} cles string keys
  * @returns {Function} Return clojure function to build the final string
- * 
+ *
  * @example
  * const { taggedString } = require("@slimio/utils");
- * 
+ *
  * const myStrClojure = taggedString`Hello ${0}!`;
  * console.log(myStrClojure("Thomas")); // stdout: Hello Thomas!
  */
@@ -24,7 +22,7 @@ function taggedString(chaines, ...cles) {
         const resultat = [chaines[0]];
         cles.forEach((cle, index) => {
             resultat.push(
-                is.number(cle) ? valeurs[cle] : dict[cle],
+                typeof cle === "number" ? valeurs[cle] : dict[cle],
                 chaines[index + 1]
             );
         });
@@ -35,5 +33,5 @@ function taggedString(chaines, ...cles) {
 
 // Exports all utils functions
 module.exports = {
-	taggedString
+    taggedString
 };
