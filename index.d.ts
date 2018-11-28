e0e5bba37c67ad1dac985d4a8f5954fce2b294fa
Malaj Alexandre <alexandre.malaj@gmail.com>
2018-11-28 18:38:11 +0100

    docs: add assertion in ts definition

--- index.d.ts
+++ index.d.ts
@@ -7,6 +7,10 @@
 declare namespace Utils {
     export function taggedString(str: string, ...keys: any[]): (...keys: any[]) => string;
     export function createDirectory(dirPath: string): void;
+    export function assertEntity(entity: object): void;
+    export function assertMIC(mic: object): void;
+    export function assertAlarm(alarm: object): void;
+    export function assertCorrelateID(CID: string): void;
 }
 
 export as namespace Utils;
